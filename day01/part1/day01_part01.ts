7e8bb59edf8328c1a3b94d339bafd109dbecbd00
bernardo <epiphonetodie@gmail.com>
2018-12-02 20:17:06 +0100

    clean up code

--- day01/part1/day01_part01.ts
+++ day01/part1/day01_part01.ts
@@ -1,28 +1,18 @@
-var fs = require("fs");
-var path = require('path');
+let fs = require("fs");
+let path = require('path');
 
-var changes = [];
-var result: number = 0;
+let changes = [];
+let result: number = 0;
 
-var filepath = path.join(__dirname, "../day01_part01_input.txt");
+let filepath = path.join(__dirname, "../day01_part01_input.txt");
 
-var text = fs.readFileSync(filepath, "utf-8");
+let text = fs.readFileSync(filepath, "utf-8");
 changes = text.split("\r\n");
 
-var newValue: number = 0;
-var tmpValue: string;
-var operator: string;
+let newValue: number = 0;
 
 for (let value of changes) {
-    operator = value.charAt(0);
-    tmpValue = value.replace(operator, "");
-
-    if (operator === "+") {
-        newValue = result + parseInt(tmpValue);
-
-    } else if (operator === "-") {
-        newValue = result - parseInt(tmpValue);
-    }
+    newValue = result + parseInt(value);
     console.log(`Current frecuency ${result}, change of ${value}; resulting frecuency ${newValue}`);
     result = newValue;
 }
--- day01/part2/day01_part02.ts
+++ day01/part2/day01_part02.ts
@@ -5,41 +5,31 @@ let changes = [];
 let result: number = 0;
 
 let filepath = path.join(__dirname, "../day01_part01_input.txt");
-// var filepath = path.join(__dirname, "./test02.txt");
+// var filepath = path.join(__dirname, "../test02.txt");
 // var filepath = path.join(__dirname, "./test03.txt");
 // var filepath = path.join(__dirname, "./test04.txt");
 
 let text = fs.readFileSync(filepath, "utf-8");
-changes = text.split("\n");
+changes = text.split("\r\n");
 
-let newValue2: number = 0;
-let tmpValue: string;
-let operator: string;
+let newValue: number = 0;
 let calculatedFrecuencies: number[] = [];
 
 let duplicatedFound: boolean = false;
 let numberofloops = 0;
 do {
     for (let value of changes) {
-        operator = value.charAt(0);
-        tmpValue = value.replace(operator, "");
-    
-        if (operator === "+") {
-            newValue2 = result + parseInt(tmpValue);
-    
-        } else if (operator === "-") {
-            newValue2 = result - parseInt(tmpValue);
-        }
-        if(calculatedFrecuencies.indexOf(newValue2) > -1) {
+        newValue = result + parseInt(value);
+        if(calculatedFrecuencies.indexOf(newValue) > -1) {
             duplicatedFound = true;
         }
-        console.log(`Current frecuency ${result}, change of ${value}; resulting frecuency ${newValue2}`);
-        result = newValue2;
-        calculatedFrecuencies.push(newValue2);
+        console.log(`Current frecuency ${result}, change of ${value}; resulting frecuency ${newValue}`);
+        result = newValue;
+        calculatedFrecuencies.push(newValue);
         if(duplicatedFound) {
             break;
         }
     }
     numberofloops++;
 } while (!duplicatedFound);
-console.log(`Final value: ${result}. Duplicated frecuency: ${newValue2}. Loops: ${numberofloops}.`);
+console.log(`Final value: ${result}. Duplicated frecuency: ${newValue}. Loops: ${numberofloops}.`);
