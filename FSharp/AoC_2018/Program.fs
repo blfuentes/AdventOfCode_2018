01ad7df2ad125c6381a4f852ef7e67cd39415d4a
bernardo <epiphonetodie@gmail.com>
2018-12-02 22:38:52 +0100

    Added day 1 with F#

---
+++ FSharp/AoC_2018/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+open day01_part01
+open day01_part02
+
+[<EntryPoint>]
+let main argv =
+    let resultPart1 = day01_part01.displayValue
+    printfn "Final result: %i" resultPart1
+    
+    let resultPart2 = day01_part02.calculateDuplicatedFrecuency
+    printfn "Duplicated frecuency: %i" resultPart2
+
+    let endprogram = Console.ReadLine()
+    0 // return an integer exit code
