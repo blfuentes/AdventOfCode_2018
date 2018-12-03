1b4102cd67944219cdbe0fd036aa7657a19792f5
bernardo <epiphonetodie@gmail.com>
2018-12-03 22:18:37 +0100

    starting day 2 with F#

--- FSharp/AoC_2018/Program.fs
+++ FSharp/AoC_2018/Program.fs
@@ -7,11 +7,15 @@ open day01_part02
 
 [<EntryPoint>]
 let main argv =
-    let resultPart1 = day01_part01.displayValue
-    printfn "Final result: %i" resultPart1
-    
-    let resultPart2 = day01_part02.calculateDuplicatedFrecuency
-    printfn "Duplicated frecuency: %i" resultPart2
 
+    // DAY 01
+    let resultday01Part1 = day01_part01.displayValue
+    printfn "Final result: %i" resultday01Part1
+    let resultday02Part2 = day01_part02.calculateDuplicatedFrecuency
+    printfn "Duplicated frecuency: %i" resultday02Part2
+
+    // DAY 02
+
+    //
     let endprogram = Console.ReadLine()
     0 // return an integer exit code
---
+++ FSharp/AoC_2018/day02/part01/day02_part01.fsx
@@ -0,0 +1,18 @@
+﻿open System.IO
+
+//let filepath = __SOURCE_DIRECTORY__ + @"../../day02_input.txt"
+let filepath = __SOURCE_DIRECTORY__ + @"./test01.txt"
+let lines = File.ReadLines(filepath)
+
+type Counter(value: string) =
+    let description = value
+    member this.numberOfAppearances = 0
+
+type Entry(value: string) =
+    let entryValue = value
+    member this.description = Array.empty<Counter>
+    member this.candidatesTwo = Array.empty<Counter>
+    member this.candidatesThree = Array.empty<Counter>
+
+let entries =
+    lines
