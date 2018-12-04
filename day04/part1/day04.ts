150b018d44626f9d2491a2573983f5b3629cde15
bernardo <epiphonetodie@gmail.com>
2018-12-04 14:40:32 +0100

    day 4 part 2

--- day04/part1/day04.ts
+++ day04/part1/day04.ts
@@ -44,6 +44,23 @@ function getMostSleepGuardValue(ids: Array<number>,  guardInfo: { [key: number]:
     return guardId * minute;
 }
 
+function getMostRepeatedSleepGuardValue(ids: Array<number>,  guardInfo: { [key: number]: GuardInfo }) : number {
+    let guardId = 0;
+    let maxMinute = 0;
+    let minute = 0;
+    for (let id of ids){
+        var tmpGuard = guardInfo[id]; 
+        var tmpMaxMinute = Math.max.apply(null, tmpGuard.minutesSleeping);
+        if (maxMinute < tmpMaxMinute) {
+            minute = tmpGuard.minutesSleeping.indexOf(tmpMaxMinute);
+            maxMinute = tmpMaxMinute;
+            guardId = id;
+        }
+    }
+
+    return guardId * minute;
+}
+
 // create events
 for (let event of entryElements) {
     var tmpEvent = new GuardEvent(event);
@@ -99,6 +116,6 @@ for (let event of guardEvents) {
     }
 }
 
-let finalResult = getMostSleepGuardValue(guardIds, guardInfo);
-
-console.log(`Result: ${finalResult}.`);
+let finalResultPart1 = getMostSleepGuardValue(guardIds, guardInfo);
+let finalResultPart2 = getMostRepeatedSleepGuardValue(guardIds, guardInfo);
+console.log(`Result part1: ${finalResultPart1}. Result part2: ${finalResultPart2}.`);
