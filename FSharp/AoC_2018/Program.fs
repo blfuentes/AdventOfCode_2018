136c2de17a15b432cc305f1348675cfc54c36a80
bernardo <epiphonetodie@gmail.com>
2018-12-05 20:02:38 +0100

    day 02 part 1 solution with F#

--- FSharp/AoC_2018/Program.fs
+++ FSharp/AoC_2018/Program.fs
@@ -5,16 +5,21 @@ open System
 open day01_part01
 open day01_part02
 
+open day02_part01
+//open day01_part02
+
 [<EntryPoint>]
 let main argv =
 
     // DAY 01
     let resultday01Part1 = day01_part01.displayValue
-    printfn "Final result: %i" resultday01Part1
+    printfn "Final result Day 01 part 1: %i" resultday01Part1
     let resultday02Part2 = day01_part02.calculateDuplicatedFrecuency
-    printfn "Duplicated frecuency: %i" resultday02Part2
+    printfn "Duplicated frecuency Day 01 part 2: %i" resultday02Part2
 
     // DAY 02
+    let resultday02Part1 = day02_part01.finalValue
+    printfn "Final result Day 02 part 1: %i" resultday02Part1
 
     //
     let endprogram = Console.ReadLine()
